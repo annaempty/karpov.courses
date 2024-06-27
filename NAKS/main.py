4c34e3afc808df6ee945dd36157305059ea1d679
Anna_w <annaempty0@gmail.com>
2024-06-27 16:19:34 +0300

    Ввод даты

--- NAKS/main.py
+++ NAKS/main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 import pandas as pd
+from select_date import find_table_by_date
 
 
 def find_table_body(html_text):
@@ -48,12 +49,12 @@ def read_data(tbody_r):
 
 
 if __name__ == '__main__':
-    option = Options()
-    option.add_argument("--disable-infobars")
-    browser = webdriver.Chrome(option)
+    # Необходимо будет вынести в текстовый файл:
     html = 'https://naks.ru/registry/reg/st/?arrSORT=&arrFilter_pf%5Bnum_acst%5D%5B%5D=3173145&arrFilter_pf%5Bnum_sv%5D=%C0%D6%D1%D2-87-&arrFilter_DATE_ACTIVE_TO_1=&arrFilter_DATE_ACTIVE_TO_2=&arrFilter_ff%5BNAME%5D=&arrFilter_ff%5BPREVIEW_TEXT%5D=&set_filter=%D4%E8%EB%FC%F2%F0&set_filter=Y'
-    browser.get(html)
-    html_text = browser.page_source
+    date_from = '01.01.2024'
+    date_to = '31.12.2024'
+
+    html_text = find_table_by_date(html, date_from, date_to)
     start_table = find_table_body(html_text)
     dict_columns = read_title(start_table)
     df = read_data(start_table)
---
+++ NAKS/select_date.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+def find_table_by_date(html, date_from, date_to):
+    option = Options()
+    option.add_argument("--disable-infobars")
+    browser = webdriver.Chrome(option)
+    browser.get(html)
+    # id первой кнопки аттестации до - arrFilter_DATE_ACTIVE_TO_1
+    elem_data_1 = browser.find_element(By.ID, 'arrFilter_DATE_ACTIVE_TO_1')
+    elem_data_1.send_keys(date_from + Keys.RETURN)
+    # id второй кнопки аттестации до -arrFilter_DATE_ACTIVE_TO_2
+    time.sleep(4)
+    elem_data_1 = browser.find_element(By.ID, 'arrFilter_DATE_ACTIVE_TO_2')
+    elem_data_1.send_keys(date_to + Keys.RETURN)
+    time.sleep(10)
+    # class кнопки фильтр inputbuttonflat
+    # На кнопку фильтр тыкать не нужно, фильтруется после ввода дат
+    # browser.find_element(By.CLASS_NAME, 'inputbuttonflat').click()
+    # time.sleep(10)
+    html_text = browser.page_source
+    return html_text
+
+
+def switch_webpage():
+    pass
